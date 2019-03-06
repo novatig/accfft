b88432cd71118b7997fa67948cb32125cb881fc1
novatig <novatig@ethz.ch>
2019-03-06 13:48:37 +0100

    Reduced by a third the memory allocated by transpose gpu plans... the only thing I give in exchange is that I cannot perform fft for one direction at the time. this could be solved by lazy allocation like I do for buffer_d2, but I don't need it for now. In practice, for most application this means I can host EIGHT TIMES bigger domains on each GPU than the original accfft (i.e. in 3D double the size in each direction that can be hosted on each GPU).

--- src/accfft_gpuf.cpp
+++ src/accfft_gpuf.cpp
@@ -49,7 +49,6 @@ inline void print_memUse(const std::string where) {
   #endif
 }
 
-
 /**
  * Cleanup all CPU resources
  */
@@ -400,7 +399,6 @@ accfft_plan_gpuf* accfft_plan_dft_3d_r2c_gpuf(int * n, float * data_d,
     plan->T_plan_yi = new T_Plan_gpu<float>(n[2], n[1], 1,
         plan->Mem_mgr, plan->row_comm, plan->isize[0]);
 
-
     plan->T_plan_1->alloc_local = plan->alloc_max;
     plan->T_plan_2->alloc_local = plan->alloc_max;
     plan->T_plan_2i->alloc_local = plan->alloc_max;
@@ -468,7 +466,6 @@ accfft_plan_gpuf* accfft_plan_dft_3d_r2c_gpuf(int * n, float * data_d,
   plan->T_plan_xi->kway_async = plan->T_plan_2->kway_async;
   plan->r2c_plan_baked = true;
   return plan;
-
 } //end accfft_plan_dft_3d_r2c_gpuf
 
 void accfft_execute_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d,
@@ -532,7 +529,6 @@ void accfft_execute_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d,
       data_out_d = data_d;
 
     // Perform N0/P0 transpose
-
     if (!plan->oneD) {
       plan->T_plan_1->execute_gpu(plan->T_plan_1, data_out_d, timings, 2,
           osize_0[0], coords[0]);
@@ -614,11 +610,9 @@ void accfft_execute_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d,
     }
 
     if (plan->oneD) {
-      plan->T_plan_2i->execute_gpu(plan->T_plan_2i, (float*) data_d,
-          timings, 1);
+      plan->T_plan_2i->execute_gpu(plan->T_plan_2i, (float*) data_d, timings, 1);
     } else {
-      plan->T_plan_2i->execute_gpu(plan->T_plan_2i, (float*) data_d,
-          timings, 1, 1, coords[1]);
+      plan->T_plan_2i->execute_gpu(plan->T_plan_2i, (float*) data_d, timings, 1, 1, coords[1]);
     }
     /**************************************************************/
     /*******************  N0/P0 x N1 x N2/P1 **********************/
@@ -689,6 +683,7 @@ void accfft_execute_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d,
     timer[3] += timings[3];
     timer[4] += timings[4];
   }
+  checkCuda_accfft (cudaDeviceSynchronize());
 
   return;
 }
@@ -1401,7 +1396,10 @@ void accfft_execute_y_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d
   int *osize_y = plan->osize_y;
   int *osize_yi = plan->osize_yi;
   size_t N_local = plan->isize[0] * plan->isize[1] * plan->isize[2];
-  float* cwork_d = plan->Mem_mgr->buffer_d3;
+  printf("For memory reasons, accfft_execute_y_gpu is not supported!\n");
+  fflush(0);
+  MPI_Abort(MPI_COMM_WORLD, 1);
+  float* cwork_d = nullptr; //plan->Mem_mgr->buffer_d3;
 
   if (direction == -1) {
     /**************************************************************/
@@ -1548,7 +1546,10 @@ void accfft_execute_x_gpuf(accfft_plan_gpuf* plan, int direction, float * data_d
   int *osize_x = plan->osize_x;
   int *osize_xi = plan->osize_xi;
   size_t N_local = plan->isize[0] * plan->isize[1] * plan->isize[2];
-  float* cwork_d = plan->Mem_mgr->buffer_d3;
+  printf("For memory reasons, accfft_execute_y_gpu is not supported!\n");
+  fflush(0);
+  MPI_Abort(MPI_COMM_WORLD, 1);
+  float* cwork_d = nullptr; //plan->Mem_mgr->buffer_d3;
 
   if (direction == -1) {
     /**************************************************************/
